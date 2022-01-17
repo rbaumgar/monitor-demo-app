3496fdf363f04dd522306570c86909477d68882b
Robert Baumgartner <rbaumgar@redhat.com>
2022-01-17 15:00:31 +0100

    update test source

--- src/test/java/org/example/rbaumgar/GreetingResourceTest.java
+++ src/test/java/org/example/rbaumgar/GreetingResourceTest.java
@@ -1,10 +1,14 @@
 package org.example.rbaumgar;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import javax.ws.rs.core.MediaType;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -15,7 +19,36 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from monitor-demo-app rbaumgar.remote.csb"));
+             .body(is("Hello from monitor-demo-app "));
+    }
+
+    @Test
+    public void test2xxEndpoint() {
+        given()
+          .when().get("/hello/2xx")
+          .then()
+             .statusCode(200)
+             .body(is("Got 2xx Response"));
+    }
+
+    @Test
+    public void test5xxEndpoint() {
+        given()
+          .when().get("/hello/5xx")
+          .then()
+             .statusCode(200)
+             .body(is("Got 5xx Response"));
+    }
+
+    @Test
+    public void testalterEndpoint() {
+        given()
+          .contentType(ContentType.JSON)
+          .body("{test}")
+          .when().post("/hello/alert-hook")
+          .then()
+             .statusCode(200)
+             .body(is("OK"));
     }
 
 }
